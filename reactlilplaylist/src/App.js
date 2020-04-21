0f831cfa63224babe482cbf93c8cd83119baba5e
Jos van Dijk <jzvndk@gmail.com>
2020-04-21 09:27:06 +0200

    Core Functionaliteiten

--- reactlilplaylist/src/App.js
+++ reactlilplaylist/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./App.css";
+// import SongForm from './SongForm';
 import SongOverview from "./SongOverview";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Lil' Playlist</h1>
-      </header>
+      <h1>React Lil' Playlist</h1>
       <SongOverview />
+      {/* <br />
+      <SongList /> */}
     </div>
   );
 }
--- reactlilplaylist/src/SongOverview.js
+++ reactlilplaylist/src/SongOverview.js
@@ -2,31 +2,39 @@ import React from "react";
 import SongForm from "./SongForm";
 import SongList from "./SongList";
 
-class SongOverview extends Component {
+class SongOverview extends React.Component {
   constructor() {
     super();
     this.state = {
       songs: [],
     };
+    this.addSong = this.addSong.bind(this);
   }
 
   addSong = (song) => {
-    // doe iets om de state aan te passen
+    console.log("ik ga iets adden");
+    const newSongs = [song, ...this.state.songs];
+    this.setState({
+      songs: newSongs,
+    });
   };
 
   render() {
     return (
       <div>
-        <SongForm addSong={this.addSong} />
-        <table style="width:100%">
-          <tr className="song-header">
-            <th className="song-row__item">Song</th>
-            <th className="song-row__item">Artist</th>
-            <th className="song-row__item">Genre</th>
-            <th className="song-row__item">Rating</th>
-          </tr>
+        <SongForm onSubmit={this.addSong} />
+
+        <table style={{ width: "100%" }}>
+          <thead>
+            <tr className="song-header">
+              <th className="song-row__header">Song</th>
+              <th className="song-row__header">Artist</th>
+              <th className="song-row__header">Genre</th>
+              <th className="song-row__header">Rating</th>
+            </tr>
+          </thead>
+          <SongList songs={this.state.songs} />
         </table>
-        <SongList songs={this.state.songs} />
       </div>
     );
   }
